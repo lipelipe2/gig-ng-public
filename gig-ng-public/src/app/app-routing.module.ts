44d483312a62b1c2a6ce61770ffd6c22fe744017
Filipe Marques <filipeavmarques@gmail.com>
2020-04-20 15:43:31 +0100

    Project Structure created

---
+++ gig-ng-public/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { GeneratorComponent } from './generator/generator.component';
+import { PaymentsComponent } from './payments/payments.component';
+
+
+const routes: Routes = [
+  { path: 'generator', component: GeneratorComponent },
+  { path: 'payments', component:PaymentsComponent },
+  { path: '',   redirectTo: 'payments', pathMatch: 'full' },
+  { path: '**', component: PaymentsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
